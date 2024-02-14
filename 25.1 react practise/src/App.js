8b339a4a39fe28da2ada90d93c86baa956297e10
rishav143 <rc8344353@gmail.com>
2024-02-14 23:39:36 +0530

    express

---
+++ 25.1 react practise/src/App.js
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+const name = "Rishav Choudhary";
+const currDate = new Date();
+const year = currDate.getFullYear();
+
+/*
+dated : 1/28/2024
+javascript expression in JSX
+export default function App() {
+  return (
+    <div className="App">
+      <p>Created by {name}</p>
+      <p>copyright {year}</p>
+    </div>
+  );
+}
+*/
+
+
